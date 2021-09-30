5a19c5d534c336165ed5d1b07fefc862d04535af
Kuba <javadeveloper123@icloud.com>
2021-09-30 20:37:17 +0200

    Dodaj pliki projektów.

---
+++ ApiTestApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ApiTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
