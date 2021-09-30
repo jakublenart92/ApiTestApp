d8b9a634b73f4cc686674d3059e90e68432be23b
Kuba <javadeveloper123@icloud.com>
2021-09-30 20:58:53 +0200

    dodana zostala instrukcja warunkowa if

--- ApiTestApp/Program.cs
+++ ApiTestApp/Program.cs
@@ -7,6 +7,16 @@ namespace ApiTestApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            int a = 0;
+
+            int b = 1;
+
+            if (int a == int b) {
+                Console.WriteLine("git")
+            } else {
+                Console.Write("dupa")
+            }
         }
     }
 }
